745f26149abe36540b6eaed5a9f511ac14d5e2f1
Minsun Kim <minsun2767@naver.com>
2016-05-26 20:54:04 +0900

    #3 Second Event Programming

--- app/src/main/java/kr/hs/emrim/minsun2767/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emrim/minsun2767/imageslideshow/MainActivity.java
@@ -4,15 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     ViewFlipper flip;
+    EditText editText;
+    int time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         flip=(ViewFlipper)findViewById(R.id.view_flip);
+        editText=(EditText)findViewById(R.id.edit_time);
         flip.setFlipInterval(1000);
         Button butStart=(Button)findViewById(R.id.but_start);
         butStart.setOnClickListener(this);
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId())
         {
             case R.id.but_start:
+                time=(int)(Double.parseDouble(editText.getText().toString())*1000);
+                flip.setFlipInterval(time);
                 flip.startFlipping();
                 break;
 
